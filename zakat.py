4200c936aee632275aaf65d8286073239752c3e0
tanzildwiramadhan <muhamadtanzildwiramadhan@gmail.com>
2025-03-26 03:46:06 +0700

    koneksi ke database MySQL

--- zakat.py
+++ zakat.py
@@ -1,17 +1,11 @@
-
+import mysql.connector
 import pandas as pd
 
-# Data global untuk harga beras dan harga menu
-harga_beras = 50000  # Harga per kilogram beras (misalnya 50.000 Rupiah per kg)
-data_zakat = []
-
-# Fungsi untuk menampilkan harga beras
-def tampilkan_harga_beras():
-    print(f"Harga beras saat ini: {harga_beras} rupiah per kilogram")
-
-# Fungsi untuk input harga menu zakat fitrah
-def input_harga_menu():
-    global harga_beras
-    harga_beras = float(input("Masukkan harga beras per kilogram (dalam rupiah): "))
-    print(f"Harga beras berhasil diupdate menjadi {harga_beras} rupiah per kilogram")
-    
+# Koneksi ke database MySQL
+def create_connection():
+    return mysql.connector.connect(
+        host="localhost",      # Alamat host MySQL
+        user="root",           # Username MySQL
+        password="",   # Password MySQL
+        database="zakat" # Nama database
+    )
